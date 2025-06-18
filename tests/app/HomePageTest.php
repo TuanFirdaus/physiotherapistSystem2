792ef52f3c2b9dcdb0ebdf414ada88fdd5eb4943
TuanFirdaus <s65650@ocean.umt.edu.my>
2025-06-19 01:21:30 +0800

    Set up CI workflow and PHPUnit test

---
+++ tests/app/HomePageTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use CodeIgniter\Test\CIUnitTestCase;
+
+final class HomePageTest extends CIUnitTestCase
+{
+    public function testTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
